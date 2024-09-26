6400f085a51c86d88c96eccb500add13694dea8a
Renan Silva Prado <renansilvaprado2@hotmail.com>
2024-09-26 15:23:09 -0300

    List comprehension com mais de um for ( e mais detalhes )

--- Progamação Funcional e  +/aula24.py
+++ Progamação Funcional e  +/aula24.py
@@ -14,3 +14,14 @@ lista = [
     for i in range(3)
 ]
 print(lista)
+
+
+#! CONTEUDO DO VIDEO DO YT ABAIXO:
+numeros = [1, 2, 3, 4, 5]
+novos_numeros = [numeros for numeros in numeros] #Numeros para numeros em numeros
+# Seria a mesa coisa que a lista de cima essa baixo
+# novos_numeros = []
+# for numeros in numeros:
+#     novos_numeros.append(numeros)
+# numeros[0] = 20
+# print(novos_numeros)
