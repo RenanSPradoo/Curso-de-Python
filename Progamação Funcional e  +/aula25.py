a3ddd4df2880830e087684e5c5fdc2dc2d3d0bfe
Renan Silva Prado <renansilvaprado2@hotmail.com>
2024-09-30 14:31:57 -0300

    Dictionary Comprehension e Set Comprehension (Não usar Tuplas)

---
+++ Progamação Funcional e  +/aula25.py
@@ -0,0 +1,26 @@
+produto = {
+    'nome': 'Caneta Azul',
+    'preço': 2.5,
+    'categoria': 'Escritorio',
+}
+
+dc = {
+    chave: valor.upper()
+    if isinstance(valor, str) else valor 
+    for chave, valor in produto.items()
+    if chave != 'categoria'
+}
+
+lista = [
+    ('a', 'valor a'),
+    ('a', 'valor a'),
+    ('a', 'valor a'),
+]
+
+dc = {
+    chave: valor
+    for chave, valor in lista
+}
+
+s1 = {2 ** i for i in range(10)}
+print(s1)
