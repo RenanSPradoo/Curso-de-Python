628e6a9afbb573d6c8daa21ea005fa711bf71d5d
Renan Silva Prado <renansilvaprado2@hotmail.com>
2024-09-30 14:48:51 -0300

    isinstace() - para saber se objeto é de determinado tipo

---
+++ Progamação Funcional e  +/aula26.py
@@ -0,0 +1,23 @@
+# isinstace = para saber se objeto é de determinado tipo
+# isinstace = é instancia de ...
+lista = [
+    'a', 1, 1.1, True, [0, 1, 2], (1, 2),
+    {0, 1}, {'nome': 'Luiz'},
+]
+
+for item in lista:
+    if isinstance(item, set):
+        print('\nSET')
+        item.add(5)
+        print(item, isinstance(item, set))
+
+    elif isinstance(item, str):
+        print('\nSTR')
+        print(item.upper())
+
+    elif isinstance(item, (int, float)):
+        print('\nNUM')
+        print(item, item * 2)
+    else:
+        print('\nOUTRO')
+        print(item)
